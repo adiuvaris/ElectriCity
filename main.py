167cc4c4e38f6fc18922b9eba330bb4402a0fc30
Lea Hodel <lea@MacBook-Pro-von-Michael.local>
2023-06-01 08:59:52 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import arcade
+
+
+class MainWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(1024, 800, "test", resizable=True)
+
+
+def main():
+    window = MainWindow()
+    # window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
