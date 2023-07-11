d7009ebdf561b4a6a231f9effffc25f097a30934
adiuvaris <121188520+adiuvaris@users.noreply.github.com>
2023-07-11 15:38:45 +0200

    Setup angepasst

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
 
     packages=setuptools.find_packages(),
-    package_dir={"": "src"},
     package_data={
         "ElectriCity": ["res/*"],
     },
