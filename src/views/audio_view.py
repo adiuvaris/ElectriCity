cd8c73d1ab3010a11e9f292a4774ee1d06322e88
adiuvaris <121188520+adiuvaris@users.noreply.github.com>
2023-09-07 14:49:14 +0200

    Audio-Ausgabe verbessert

--- src/views/audio_view.py
+++ src/views/audio_view.py
@@ -6,6 +6,7 @@ import arcade.gui
 import src.const as const
 from src.data.game import gd
 from src.data.media import Media
+from src.ui.attributed_text import AttributedText
 
 
 class AudioView(arcade.View):
@@ -79,8 +80,30 @@ class AudioView(arcade.View):
 
         # Solange die Ausgabe läuft, keine Tasten akzeptieren
         if self.sound is not None:
+
+            if key == arcade.key.KEY_0 or key == arcade.key.NUM_0:
+                self.media_player.seek(0.0)
+
             if self.sound.is_playing(self.media_player):
+
+                if key == arcade.key.SPACE:
+                    self.media_player.pause()
+
+                if (key == arcade.key.UP or key == arcade.key.NUM_UP or
+                        key == arcade.key.RIGHT or key == arcade.key.NUM_RIGHT):
+                    if self.media_player.volume < 1.0:
+                        self.media_player.volume = self.media_player.volume + 0.1
+
+                if (key == arcade.key.DOWN or key == arcade.key.NUM_DOWN or
+                        key == arcade.key.LEFT or key == arcade.key.NUM_LEFT):
+                    if self.media_player.volume > 0.1:
+                        self.media_player.volume = self.media_player.volume - 0.1
+
                 return
+
+            else:
+                if key == arcade.key.SPACE:
+                    self.media_player.play()
 
         # Escape geht zurück zur aufrufenden View
         if key == arcade.key.ESCAPE:
@@ -89,7 +112,6 @@ class AudioView(arcade.View):
     def create_ui(self):
         """
         User-Interface erstellen - ein Button pro Memory-Karte
-        :param ui_manager: Arcade UIManager
         """
 
         # Zuerst mal Elemente löschen
@@ -107,6 +129,15 @@ class AudioView(arcade.View):
                                    font_size=gd.scale(const.FONT_SIZE_H1),
                                    multiline=False)
         self.manager.add(titel.with_border())
+
+        hinweis = ["Die Ausgabe kann mit der <i><b>Leertaste</b></i> pausiert werden. Mit der Taste <i><b>Null</b></i> beginnt das Gespräch von vorne.",
+                   "Mit den <i><b>Pfeiltasten</b></i> kann die Lautstärke angepasst werden."]
+        text = AttributedText(x=gd.scale(20), y=gd.scale(20),
+                              width=gd.scale(1240),
+                              height=gd.scale(80),
+                              text=hinweis)
+
+        self.manager.add(text)
 
         # Bild Element erzeugen - falls Datei existiert
         if self.media.illustration != "":
